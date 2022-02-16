e99ee59e1e9dd18d26ee0f462eb6dd45f9fb7bca
Hugo Leroux <hugolerouxmusic@gmail.com>
2022-02-16 13:20:19 +0100

    dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const app = express();
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('groupomania', 'root', 'L@GrandeArm33', {
-     host: 'localhost',
-     port: "3306",
-     dialect: 'mysql'
-});
 const userRoutes = require('./routes/user.js');
 const publicationRoutes = require('./routes/publication.js');
+const Sequelize = require('sequelize');
+
+require('dotenv').config({path: '../.env'});
+
+const sequelize = new Sequelize('groupomania', process.env.sequelizeUser, process.env.sequelizePassword, {
+     dialect: 'mysql',
+     port: '3306'
+});
+
+sequelize.authenticate().then( ()=> {
+     console.log('connexion réussie :)')
+}) .catch(() => {
+     console.log('connexion échouée :\'(')
+});
 
 app.use(express.json());
-
-async () => {
-     try {
-          await sequelize.authenticate();
-          console.log('Connection has been established successfully.');
-     } catch (error) {
-          console.error('Unable to connect to the database:', error);
-     }
-}
 
 app.use("/api/user", userRoutes);
 app.use("/api/publication", publicationRoutes);
 
-module.exports = app
+module.exports = app;
